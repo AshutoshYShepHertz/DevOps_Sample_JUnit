cba773fa0199def87eff1bda946c93de3f311fb3
ashutosh.yadav <ashutosh.yadav@shephertz.com>
2015-08-17 10:12:50 +0530

    Addign Junit
    Signed-off-by: ashutosh.yadav <ashutosh.yadav@shephertz.com>

--- src/test/java/JunitTest/JunitTest.java
+++ src/test/java/JunitTest/JunitTest.java
@@ -16,28 +16,8 @@ import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class JunitSeleniumTest {
-    @Test
-    public void myTest() throws Exception {
-    	
-    	WebDriver driver = new RemoteWebDriver(
-                                new URL("http://app42seleniumipaddress/wd/hub/"), 
-                                DesiredCapabilities.firefox());
-        
-        driver.get("http://www.google.com");
-       
-        
-        // RemoteWebDriver does not implement the TakesScreenshot class
-        // if the driver does have the Capabilities to take a screenshot
-        // then Augmenter will add the TakesScreenshot methods to the instance
-        WebDriver augmentedDriver = new Augmenter().augment(driver);
-        File screenshot = ((TakesScreenshot)augmentedDriver).
-                            getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshot, new File("/home/paasadmin/testimage.png"));
-        Assert.assertEquals(driver.getTitle(), "Google");
-		System.out.println("Selenium Test Successfull");
-        
-    }
+public class JunitTest {
+   
 	@Test
     public void myTest2() {
 		Assert.assertEquals("Google", "Google");
